8bf13d13a551824ce87ad84ba8472dc903a5c45b
zfalbayrak@gmail.com <zfalbayrak@gmail.com>
2024-06-28 20:30:49 +0300

    Home page card view

--- eCommerceWeb/Models/Brand.cs
+++ eCommerceWeb/Models/Brand.cs
@@ -8,9 +8,9 @@ namespace eCommerceWeb.Models
         [Key]
         public int Id { get; set; }
 
-        [Display(Name="Marka İsmi")]
+        [Display(Name="Marka Adı")]
         [Required(ErrorMessage = "Ad kısmı eksik bırakılamaz")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Tam ad 3-50 karakter arasında olmalıdır...")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Marka adı 3-50 karakter arasında olmalıdır...")]
         public string Name { get; set; }
 
         [Display(Name = "Marka Hakkında")]
